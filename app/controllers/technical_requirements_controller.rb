2e59a2933e4df5f3c5c4c48b9a9840ec0703a1c9
DimaFursov <dimafursov@gmail.com>
2020-09-30 16:37:30 +0300

    tech req Garage

--- app/controllers/technical_requirements_controller.rb
+++ app/controllers/technical_requirements_controller.rb
@@ -68,9 +68,10 @@ class TechnicalRequirementsController < ApplicationController
   #7. get list of tasks having several exact matches of both name and status,
   #   from the project 'Garage'. Order by matches count
   def tasks_exact_matches_both_name_status_from_project_name_Garage  
-  #'projects.name = ?', 'Garage'  
+  #projects = Project.includes(:tasks).unscoped.where('projects.name = ?', 'Garage').map do |project| project.tasks.unscope(:order).group(:name, :status).order('count_name desc').having('count(name) > 1').count(:name) end
     projects = Project.includes(:tasks).unscoped.where('projects.name = ?', 'Garage').map do |project|
-      count_group_name_status = project.tasks.unscope(:order).group(:name, :status).order('count_name desc').having('count_name > 1').count(:name)
+      count_group_name_status = project.tasks.unscope(:order).group(:name, :status).order('count(name) desc').having('count(name) > 1').count(:name)
+      # pg count(name)
       {
         project_name: project.name,
         garage_tasks_count_desc: count_group_name_status
@@ -81,39 +82,19 @@ class TechnicalRequirementsController < ApplicationController
 
   #8. - get the list of project names having more than 10 tasks in status'completed'. Order by project_id
   def list_project_more_10_tasks_true
-
 =begin    
     render json: Project.find_by_sql("
       SELECT p.name 
       FROM projects p WHERE EXISTS (SELECT `project_id` FROM tasks t 
       WHERE p.id=t.project_id GROUP BY `project_id` AND t.status='true' HAVING count(*)>10) ORDER BY p.id ASC")  
 =end
-
     projects = Project.includes(:tasks).unscoped.map do |project|
-      s = project.tasks.unscope(:order).group(:status).order('count_status desc').having('count_status > 10').count(:status)
-      a = project.tasks.unscope(:order).group(:status).order(:project_id).having('count_status > 10').count(:status)
-      b = project.tasks.unscope(:order).group(:status).order(:project_id).having(status: true).having('count_status > 10').count(:status)
       
-      #q = project.tasks.limit(11).group(:status).order('task_status desc').having('task_status > 10').count(:status)
-    #.unscope(:order).group(:name, :status).order('count_name desc').having('count_name > 1').count(:name)      
-    #Project.unscoped.includes(:tasks).unscoped.map do |project| project.tasks.unscope(:order).group(:name, status: true).order('count_status desc').having('count_status > 10').count(:status) end
-    #Project.unscoped.includes(:tasks).unscoped.map do |project| project.tasks.unscope(:order).having(status: true).order('count_name desc').limit(11) end
-      # if Task.where('tasks.project_id = ?', project.id).limit(11).count(status: true) > 10 then
-      # if project.tasks.limit(11).count(status: true) > 10 
-      #Task.unscoped.where('status = ?', 'true')
-      #Task.where('tasks.project_id = ?', project.id).limit(11).count(status: true)
-      #q = project.tasks.group(:status).having(status: true)#.count(:status)
-      #if project.tasks.where(status: true).count(:status) > 10
-      #c = project.tasks.where(status: true).count(:status)
-      #if hash(s) > 10 then
+      a = project.tasks.unscope(:order).group(:status).order(:project_id).having('count_status > 10').count(:status)
+      
       {
-        #project_id: project.id,
         project_name: project.name,
-        #pr_c_t: project.tasks.count,
-        a: a,
-        b: b
-        #q: q,
-        #c: sc #0
+        a: a
       }
       #end
     end
